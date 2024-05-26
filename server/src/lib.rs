8da409989c75d20b11088b19906f1df1db0499d2
Julien Lavocat <julien.lavocat@gmail.com>
2024-05-26 19:13:42 +0200

    feat: add server

---
+++ server/src/lib.rs
@@ -0,0 +1,11 @@
+use spacetimedb::{spacetimedb, ReducerContext};
+
+#[spacetimedb(table)]
+pub struct WorldInfos {
+    seed:
+}
+
+#[spacetimedb(connect)]
+pub fn identity_connected(ctx: ReducerContext) {
+
+}
